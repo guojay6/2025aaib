a1f00c962f9112876b455c5035577b56032045ae
guojay6 <zhongjieguo777@gmail.com>
2025-05-21 10:50:45 +0800

    week 14

---
+++ week14-1.cpp
@@ -0,0 +1,18 @@
+
+//week14-1.cpp
+#include <stdio.h>
+
+int main()
+{
+	int a[100];
+	int N, t=1;
+	while( scanf("%d", &N) == 1){//part1
+		for(int i=0 ;i<N; i++){//part1:Input
+			scanf("%d", &a[i] );//part1:Input
+		 }
+
+		 //part2 :output
+		 printf("Case #%d: It is a B2-Sequence.\n\n",t);
+		 t++;
+		}
+}
